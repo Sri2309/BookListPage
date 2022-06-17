3baa79a42ae6ba53a1cc6133a68bc628b43a30e1
Nainika Yalamati <kasaranenisri@gmail.com>
2022-06-17 16:53:24 +0400

    updated file with lint

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor(private http:HttpClient) {}
-    
-  postBook(data:any){
-  
-	 return this.http.post<any>("http://localhost:3000/bookList/",data);
-   }
-   getBook(){
-    return this.http.get<any>("http://localhost:3000/bookList/");
-   }
-	 
-  putBook(data:any,id:number){
-  return this.http.put<any>("http://localhost:3000/bookList/"+ id,data);
+  constructor(private http: HttpClient) { }
+  postBook(data: any) {
+    return this.http.post<any>("http://localhost:3000/bookList/", data);
   }
-  
-  deleteBook(id:number){
-  return this.http.delete<any>("http://localhost:3000/bookList/"+ id);
+  getBook() {
+    return this.http.get<any>("http://localhost:3000/bookList/");
+  }
+  putBook(data: any, id: number) {
+    return this.http.put<any>("http://localhost:3000/bookList/" + id, data);
+  }
+  deleteBook(id: number) {
+    return this.http.delete<any>("http://localhost:3000/bookList/" + id);
   }
 }
