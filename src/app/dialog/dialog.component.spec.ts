f566734357518d8cd562b7c5f0352ffbac798e0f
Nainika Yalamati <kasaranenisri@gmail.com>
2022-06-17 16:56:49 +0400

    updated lint chnages

--- src/app/dialog/dialog.component.spec.ts
+++ src/app/dialog/dialog.component.spec.ts
@@ -1,22 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DialogComponent } from './dialog.component';
-
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DialogComponent ]
+      declarations: [DialogComponent]
     })
-    .compileComponents();
-
+      .compileComponents();
     fixture = TestBed.createComponent(DialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
